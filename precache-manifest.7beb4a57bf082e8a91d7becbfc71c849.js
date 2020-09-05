18dfd1d8fb244341300d13ea46000c192448b43a
afrouz <afrouzhakim@gmail.com>
2020-09-05 10:33:27 +0200

    Updates

---
+++ precache-manifest.7beb4a57bf082e8a91d7becbfc71c849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff27d21fa6475989576179347eddaac",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "1cd11f1aeeacbd75c081",
+    "url": "/todo-list-app/static/css/main.fe13c229.chunk.css"
+  },
+  {
+    "revision": "c852a8693ee385c3b421",
+    "url": "/todo-list-app/static/js/2.c817cc59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-app/static/js/2.c817cc59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd11f1aeeacbd75c081",
+    "url": "/todo-list-app/static/js/main.8b1e7c16.chunk.js"
+  },
+  {
+    "revision": "fc1ebefe855e12ff14a2",
+    "url": "/todo-list-app/static/js/runtime-main.d4521ec4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
