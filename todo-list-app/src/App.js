ce1cd7bba61fe139e9c2dfaeeb4268d471dc18c9
afrouz <afrouzhakim@gmail.com>
2020-09-05 10:19:30 +0200

    set the url

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -71,6 +71,7 @@ class App extends React.Component {
   return (
     <div className="App">
       <header>
+        <p id="listheader">Daily to-do-list App</p>
         <form id="to-do-form" onSubmit={this.addItem}>
           <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
           <button type="submit">Add</button>
